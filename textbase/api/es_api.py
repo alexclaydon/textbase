5291e76e5186b6137a17f6e74a32f5590a6e1359
Alex Claydon <alexclaydon@gmail.com>
2020-07-29 16:36:44 +0900

    Additional API convenience helper functions, to be later rolled into the CLI

--- textbase/api/es_api.py
+++ textbase/api/es_api.py
@@ -1,7 +1,18 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Document, Integer, Keyword, Text
+from elasticsearch_dsl.response import Response
+from elasticsearch.exceptions import NotFoundError
+import pprint
+
+from elasticsearch_dsl.connections import connections
+
+connections.create_connection(hosts=['192.168.2.100'])
+
+# Using ```connections.create_connection``` instead of ```es = Elasticsearch ...``` has one big advantage: namely that when you are interacting with the ES instance using the class defined here (ESArticle, itself subclassed from Document), it will know where to look for the connection in the environment; if you use the other approach instead then, for example, when you try to return an ESArticle using the ESArticle.get() method it will throw a "KeyError: "There is no connection with alias 'default'."
 
 es = Elasticsearch(hosts='192.168.2.100')
+
+pp = pprint.PrettyPrinter(indent=4)
 
 # Define a subclass based on elasticsearch.Document
 
@@ -26,35 +37,131 @@ class ESArticle(Document):
         return super(ESArticle, self).save(** kwargs)
 
 
-def get_all_existing_articles_from_es(connection=es):
-    res = connection.search(index="articles", body={"query": {"match_all": {}}})
-    results = set()
-    for hit in res['hits']['hits']:
-        results.add(hit['_source']['url'])
-    return results
+# def get_all_existing_articles_from_es(connection=es):
+#     res = connection.search(index="articles", body={"query": {"match_all": {}}})
+#     results = set()
+#     for hit in res['hits']['hits']:
+#         results.add(hit['_source']['url'])
+#     return results
 
 
 def new_get_all_existing_urls_from_es():
-    return set([item['url'] for item in Search(using=es, index="articles").scan()])
+    return set(
+        [item['url'] for item in Search(
+            using=es,
+            index="articles"
+        ).scan()]
+    )
 
 
-def search_existing_articles_for_text(text: str, connection=es):
-    s = Search(using=connection, index="articles").query("match", body=text)
-    response = s.execute()
-    for hit in response:
-        print(hit.meta.score, hit.title, hit.url)
+def search_existing_articles_by_text(
+        text: str,
+        connection=es
+):
+    s = Search(
+        using=connection,
+        index="articles"
+    ).query(
+        "match",
+        body=text
+    )
+    return s.execute()
+
+
+def search_existing_articles_by_url(
+        url: str,
+        connection=es
+):
+    s = Search(
+        using=connection,
+        index="articles"
+    ).query(
+        "match",
+        url=url
+    )
+    return s.execute()
+
+
+def search_existing_articles_by_authors(
+        authors: str,
+        connection=es
+):
+    s = Search(
+        using=connection,
+        index="articles"
+    ).query(
+        "match",
+        authors=authors
+    )
+    return s.execute()
+
+
+def search_existing_articles_by_title(
+        title: str,
+        connection=es
+):
+    s = Search(
+        using=connection,
+        index="articles").query(
+        "match", title=title
+    )
+    return s.execute()
+
+
+def pp_search_result(result: Response):
+    for hit in result:
+        pp.pprint(
+            [
+                hit.meta.score,
+                hit.meta.id,
+                hit.title,
+                hit.url
+            ]
+        )
+
+
+def pp_top_search_result_detailed(result: Response):
+    hit = result.hits[0]
+    pp.pprint(
+        [
+            f'Score: {hit.meta.score}',
+            f'ID: {hit.meta.id}',
+            f'Title: {hit.title}',
+            f'Authors: {hit.authors}',
+            f'URL: {hit.url}',
+            f'Body: {hit.body}',
+        ]
+    )
 
 
 def delete_all_articles():
-    es.delete_by_query(index='articles', body={"query": {"match_all": {}}})
+    es.delete_by_query(
+        index='articles',
+        body={
+            "query": {
+                "match_all": {}
+            }
+        }
+    )
+
+
+def return_article_by_id(id):
+    try:
+        return es.get(index='articles', id=id)
+    except NotFoundError:
+        print(f'Article with ID {id} not found')
+
+
+def retrieve_article_as_object_by_id(id):
+    article = ESArticle.get(id=id)
+    return article
+
+
+def delete_article_by_id(id: str):
+    es.delete(index='articles', id=id)
 
 
 #### Scratchpad #####
-# 
-# Retrieve article as a Python object (using ID)
-
-# article = ESArticle.get(id=42)
-# print(article.title, article.authors)
 
 # Search all articles directly using the API
 #
@@ -64,18 +171,3 @@ def delete_all_articles():
 # for hit in res['hits']['hits']:
 #     print("%(title)s %(authors)s %(url)s" % hit["_source"])
 #
-# # Search for words directly using the API
-#
-# s = Search(using=es)
-# s.query("match", body="culture")
-# response = s.execute(ignore_cache=True)
-# for hit in response['hits']:
-#     print(hit.title)
-#
-#
-# s = Search(using=es, index="articles").query("match", body="raspberry pi camera")
-#
-# response = s.execute()
-#
-# for hit in response:
-#     print(hit.meta.score, hit.title, hit.url)
